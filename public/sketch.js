c01557690d39a20abb6c9758d1aec6bfe3849a64
Charlie Brennan <charliebrennan50@gmail.com>
2020-10-28 19:18:17 -0400

    Update

--- public/sketch.js
+++ public/sketch.js
@@ -2,9 +2,12 @@
 var Engine = Matter.Engine,
     World = Matter.World,
     Bodies = Matter.Bodies;
+    Sleeping = Matter.Sleeping;
 
 // create an engine
-var engine = Engine.create();
+var engine = Engine.create({
+    enableSleeping: true
+});
 let bodies = [];
 let pegs = [];
 let ballCounter = 0;
@@ -24,8 +27,8 @@ function setup() {
     leftSide = new M_Boundary(0, 500, 60, 1000);
 
     // Bottom Bins
-    for (let i=1; i<11; i++) {
-        bottomBoundaries.push(new M_Boundary(i*width/10, height-100, 10, 200));
+    for (let i = 1; i < 11; i++) {
+        bottomBoundaries.push(new M_Boundary(i * width / 10, height - 100, 10, 200));
     }
 
     // Make Pegs
@@ -34,8 +37,8 @@ function setup() {
     for (let j = 2; j < rows - 1; j++) {
         for (let i = 0; i < cols; i++) {
             pegs.push(new M_Peg(
-                j % 2 ? i * width / cols + width / (2 * cols): i * width / cols,
-                j * height / (1.25*rows),
+                j % 2 ? i * width / cols + width / (2 * cols) : i * width / cols,
+                j * height / (1.25 * rows),
                 10));
         }
     }
@@ -47,10 +50,11 @@ function draw() {
 
     frameCount++;
     if (frameCount % 10 == 0) {
-        if (ballCounter < 200) {
-            let x = Math.floor(Math.random() * (405-395) ) + 395;
+        if (ballCounter < 100) {
+            let x = Math.floor(Math.random() * (405 - 395)) + 395;
             bodies.push(new M_Circle(x, 30, 10, {
-                friction: 0
+                friction: 0,
+                restitution: 0.1
             }));
             ballCounter++;
         }
@@ -63,6 +67,7 @@ function draw() {
 
     for (b of bodies) {
         b.show();
+        b.body.velocity.x == 0 && Sleeping.set.b: true;
     }
 
     for (p of pegs) {
@@ -71,6 +76,13 @@ function draw() {
 
     for (b of bottomBoundaries) {
         b.show();
+    }
+
+    for (var i = 0; i < bodies.length; i++) {
+        Matter.Events.on(bodies[i], 'sleepStart sleepEnd', function(event) {
+            var body = this;
+            console.log('body id', body.id, 'sleeping:', body.isSleeping);
+        });
     }
 
     Engine.update(engine);
